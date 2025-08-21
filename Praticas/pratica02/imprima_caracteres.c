4881c396b7168bc5547e5dc2f8f53829b16d3bbf
gabriel-cps <g.carlos@iesb.edu.br>
2025-08-20 21:51:37 -0300

    mudancas

---
+++ Praticas/pratica02/imprima_caracteres.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    printf("%c\n", 'A');
+
+    printf("%i\n", 'A');
+
+    printf("%s\n", "Eu estou aprendendo a programar em C!");
+
+    printf("%20s\n", "Cafe");
+    printf("%-20s\n", "Cafe");
+    
+    return 0;
+}
---
+++ Praticas/pratica02/imprima_seu_nome.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("\nGabriel Carlos Pereira da Silva\n");
+
+    return 0;
+}
